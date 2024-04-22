b8c8260c4a83e940e2fbf64680abfac846cec0a8
Shashank Lama <supershank53@gmail.com>
2024-04-22 22:18:04 +0545

    changed css of view items page

--- feedbackapp/views.py
+++ feedbackapp/views.py
@@ -1,28 +1,3 @@
-# from django.shortcuts import render
-# from .models import Feedback
-# from django.shortcuts import HttpResponse
-
-# def GiveFeedback(request):
-#     if request.method == 'POST':
-#         feedbackemail = request.POST.get('email')
-#         feedbacktext = request.POST.get('feedbacktext')
-#         rating = request.POST.get('rating')
-
-#         print(f"Feedback email: {feedbackemail}")
-#         print(f"Feedback text: {feedbacktext}")
-#         print(f"Rating: {rating}")
-
-#         try:
-#             feedback_instance = Feedback.objects.create(
-#                 feedbackemail=feedbackemail,
-#                 feedbacktext=feedbacktext,
-#                 rate=rating
-#             )
-#             print(f"Feedback instance created: {feedback_instance}")
-#         except Exception as e:
-#             print(f"Error creating feedback: {e}")
-
-#     return render(request, 'feedback.html')
 
 from django.shortcuts import render
 from .models import Feedback
